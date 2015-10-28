baa98582b649364b6e867df6a926a7fd18ffefeb
Stefan <swp5ab@virginia.edu>
2015-10-28 18:14:38 -0400

    Changed 2nd Window to JDialog with modality(true)

--- src/Methods.java
+++ src/Methods.java
@@ -1,51 +1,52 @@
 import java.awt.Color;
+import java.awt.Dialog;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
-public class Methods { 
+public class Methods {
 	/*
-	 * This is where all of the methods will go for the buttons
-	 * The first method below decides what the button will do on click
-	 * in this case it opens a new window
-	 * 
+	 * This is where all of the methods will go for the buttons The first method
+	 * below decides what the button will do on click in this case it opens a
+	 * new window
 	 */
-	
-	public static void newWindowOnClick(JButton b){
-		b.addActionListener(new ActionListener(){
+
+	public static void newWindowOnClick(JButton b) {
+		b.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JFrame window = new JFrame();
+				JDialog dialog = new JDialog();
+				dialog.setLocation(200, 200);
+				dialog.setModal(true);
 				JPanel panel = new JPanel();
-				panel.setLayout(new GridLayout(3,3,1,1));
-				panel.setBackground(Color.black);
 				Methods.makeNineButtons(panel);
-				window.add(panel);
-				window.setSize(400,400);
-				window.setLocation(200,200);
-				window.setVisible(true);
-				
-				
+				panel.setBackground(Color.white);
+				panel.setLayout(new GridLayout(3, 3));
+				dialog.setSize(400, 400);
+				dialog.add(panel);
+				dialog.setVisible(true);
+
 			}
-			
+
 		});
-		
+
 	}
+
 	/*
 	 * This method just adds nine buttons to the gridlayout of the jpanel
 	 */
-	public static void makeNineButtons(JPanel p){
-		for(int x =0; x<9; x++){
+	public static void makeNineButtons(JPanel p) {
+		for (int x = 0; x < 9; x++) {
 			JButton b = new JButton("");
 			b.setBackground(Color.white);
 			p.add(b);
-			}
+		}
 	}
 
 }
