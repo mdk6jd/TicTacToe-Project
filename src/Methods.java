cda420f94b12e41b3981c8f0180298e9eb753b0b
Stefan <swp5ab@virginia.edu>
2015-10-29 00:24:16 -0400

    Added X's, Make sure u DL exes.png

--- src/Methods.java
+++ src/Methods.java
@@ -1,15 +1,18 @@
 import java.awt.Color;
-import java.awt.Dialog;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Methods {
+	private static ArrayList<JButton> listOButtons = new ArrayList<JButton>();
+	
 	/*
 	 * This is where all of the methods will go for the buttons The first method
 	 * below decides what the button will do on click in this case it opens a
@@ -21,17 +24,21 @@ public class Methods {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				
 				JDialog dialog = new JDialog();
+				
+
 				dialog.setLocation(200, 200);
 				dialog.setModal(true);
 				JPanel panel = new JPanel();
 				Methods.makeNineButtons(panel);
 				panel.setBackground(Color.white);
 				panel.setLayout(new GridLayout(3, 3));
+				Methods.playGame();
 				dialog.setSize(400, 400);
 				dialog.add(panel);
 				dialog.setVisible(true);
-
+				
 			}
 
 		});
@@ -42,11 +49,56 @@ public class Methods {
 	 * This method just adds nine buttons to the gridlayout of the jpanel
 	 */
 	public static void makeNineButtons(JPanel p) {
+		
 		for (int x = 0; x < 9; x++) {
 			JButton b = new JButton("");
 			b.setBackground(Color.white);
 			p.add(b);
+			listOButtons.add(b);
 		}
 	}
+	/*
+	 * This is where i'll change the icons on click
+	 * for now this is where i will play the game, but this will probably end up being placed
+	 * in another class
+	 */
+	public static void playGame(){
+	
+		
+		for(int x =0; x<listOButtons.size(); x++){
+			
+			
+			
+			JButton temp = listOButtons.get(x);
+			
+			temp.addActionListener(new ActionListener(){
+				
+				Icon img = new ImageIcon("exes.png");
+				int count = 0;
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					count++;
+					
+					if(count>0){
+						
+						temp.setIcon(img);
+						temp.setDisabledIcon(img);
+						temp.setEnabled(false);
+						
+						
+					}
+					System.out.print(count);
+				
+					
+				}
+				
+			});
+			
+			}
+		}
+	
+	
 
-}
+	}
+
