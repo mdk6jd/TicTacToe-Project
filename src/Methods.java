98f39586c5aed04dc12f7a31cbd47aac3d067845
Stefan <swp5ab@virginia.edu>
2015-10-31 17:34:00 -0400

    A lot of Stuff
    We need to add some opening and closing game buttons and just update the main screen. Other than that, the game works for each of the 9 squares a new set of nine squares show up. It knows when you win because I've kept track of the position in the array list. I'll comment the code later to explain whats goin on.

--- src/Methods.java
+++ src/Methods.java
@@ -2,16 +2,23 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class Methods {
 	private static ArrayList<JButton> listOButtons = new ArrayList<JButton>();
+	private static JPanel pan = new JPanel();
+	private static JDialog dia = new JDialog();
+	protected static int checkforwin = 0;
 
 	/*
 	 * This is where all of the methods will go for the buttons The first method
@@ -20,23 +27,25 @@ public class Methods {
 	 */
 
 	public static void newWindowOnClick(JButton b) {
+
 		b.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 
-				JDialog dialog = new JDialog();
+				dia.setLocation(200, 200);
+				dia.setModal(true);
 
-				dialog.setLocation(200, 200);
-				dialog.setModal(true);
-				JPanel panel = new JPanel();
-				Methods.makeNineButtons(panel);
-				panel.setBackground(Color.white);
-				panel.setLayout(new GridLayout(3, 3));
+				Methods.makeNineButtons(pan, dia);
+				pan.setBackground(Color.white);
+				pan.setLayout(new GridLayout(3, 3));
 				Methods.playGame();
-				dialog.setSize(400, 400);
-				dialog.add(panel);
-				dialog.setVisible(true);
+
+				dia.setSize(400, 400);
+				dia.add(pan);
+
+				dia.setVisible(true);
+				Methods.closeDialog4Win(dia);
 
 			}
 
@@ -47,17 +56,30 @@ public class Methods {
 	/*
 	 * This method just adds nine buttons to the gridlayout of the jpanel
 	 */
-	public static void makeNineButtons(JPanel p) {
+	public static void makeNineButtons(JPanel p, JDialog d) {
 
 		for (int x = 0; x < 9; x++) {
 			JButton b = new JButton("");
 			b.setBackground(Color.white);
 			p.add(b);
 			listOButtons.add(b);
+
 		}
+		d.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				p.removeAll();
+				p.updateUI();
+				listOButtons.clear();
+				posx.clear();
+				poso.clear();
+			}
+		});
+
 	}
 
-	protected static int count;
+	protected static int count = 0;
+	protected static ArrayList<Integer> poso = new ArrayList<Integer>();
+	protected static ArrayList<Integer> posx = new ArrayList<Integer>();
 
 	/*
 	 * This is where i'll change the icons on click for now this is where i will
@@ -69,6 +91,7 @@ public class Methods {
 		for (int x = 0; x < listOButtons.size(); x++) {
 
 			JButton temp = listOButtons.get(x);
+			int pos = x;
 
 			temp.addActionListener(new ActionListener() {
 
@@ -84,6 +107,7 @@ public class Methods {
 						temp.setIcon(img);
 						temp.setDisabledIcon(img);
 						temp.setEnabled(false);
+						posx.add(pos);
 
 					}
 					if (count > 0 && count % 2 == 0) {
@@ -91,16 +115,104 @@ public class Methods {
 						temp.setIcon(img);
 						temp.setDisabledIcon(img2);
 						temp.setEnabled(false);
+						poso.add(pos);
 
 					}
 					System.out.print(count);
+					System.out.println("Position x: " + posx + "Position o: "
+							+ poso);
+
+					if ((posx.contains(1) && posx.contains(2) && posx
+							.contains(0))
+							|| (posx.contains(0) && posx.contains(3) && posx
+									.contains(6))
+							|| (posx.contains(0) && posx.contains(4) && posx
+									.contains(8))
+							|| (posx.contains(1) && posx.contains(4) && posx
+									.contains(7))
+							|| (posx.contains(2) && posx.contains(4) && posx
+									.contains(6))
+							|| (posx.contains(2) && posx.contains(5) && posx
+									.contains(8))
+							|| (posx.contains(3) && posx.contains(4) && posx
+									.contains(5))
+							|| (posx.contains(6) && posx.contains(7) && posx
+									.contains(8))) {
+						JDialog dialog = new JDialog();
+						dialog.setSize(200, 100);
+						JLabel winstatus = new JLabel();
+						winstatus.setText("It seems the X's have won!");
+						winstatus.setHorizontalAlignment(SwingConstants.CENTER);
+						dialog.add(winstatus);
+						dialog.addWindowListener(new WindowAdapter() {
+							public void windowClosing(WindowEvent e) {
+								pan.removeAll();
+								pan.updateUI();
+								posx.clear();
+								poso.clear();
+								listOButtons.clear();
+								dia.dispose();
+							}
+						});
+						posx.clear();
+						poso.clear();
+
+						dialog.setModal(true);
+						dialog.setVisible(true);
+						count = 0;
+
+					} else if ((poso.contains(1) && poso.contains(2) && poso
+							.contains(0))
+							|| (poso.contains(0) && poso.contains(3) && poso
+									.contains(6))
+							|| (poso.contains(0) && poso.contains(4) && poso
+									.contains(8))
+							|| (poso.contains(1) && poso.contains(4) && poso
+									.contains(7))
+							|| (poso.contains(2) && poso.contains(4) && poso
+									.contains(6))
+							|| (poso.contains(2) && poso.contains(5) && poso
+									.contains(8))
+							|| (poso.contains(3) && poso.contains(4) && poso
+									.contains(5))
+							|| (poso.contains(6) && poso.contains(7) && poso
+									.contains(8))) {
+						JDialog dialog = new JDialog();
+						dialog.setSize(200, 100);
+						JLabel winstatus = new JLabel();
+						winstatus.setText("It seems the O's have won!");
+						winstatus.setHorizontalAlignment(SwingConstants.CENTER);
+						dialog.add(winstatus);
+						dialog.addWindowListener(new WindowAdapter() {
+							public void windowClosing(WindowEvent e) {
+								pan.removeAll();
+								pan.updateUI();
+								posx.clear();
+								poso.clear();
+								listOButtons.clear();
+								dia.dispose();
+							}
+						});
+						posx.clear();
+						poso.clear();
+
+						dialog.setModal(true);
+						dialog.setVisible(true);
+						count = 1;
+
+					}
 
 				}
 
 			});
 
 		}
-		
+
+	}
+
+	public static void closeDialog4Win(JDialog d) {
+		d.dispose();
+
 	}
 
 }
